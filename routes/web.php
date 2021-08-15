e9ebdc4cf9664b8c8a3b147b2b127841854b3f43
kzan1410 <taweesaknuanjampa@gmail.com>
2021-08-16 01:35:35 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//----------**********----------//
+//----------System----------//
+//----------**********----------//
+
 //run to create permission
 Route::get('/FirstRun', 'FirstRunController@run');
-  
+
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-  
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+
+//----------**********----------//
+//----------FontEnd----------//
+//----------**********----------//
+ 
+
 Route::get('profile', function () {
     return view('fontend/user-profile');
 })->middleware('auth'); 
@@ -32,27 +45,75 @@ Route::get('my-subject', function () {
     return view('fontend/user-subject');
 });
   
+ 
+  
+//----------**********----------//
+//----------FontEnd/student----------//
+//----------**********----------//
+  
 
-//route of admin user page
+//----------**********----------//
+//----------FontEnd/teacher----------//
+//----------**********----------//
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+
+//----------**********----------//
+//----------BackEnd----------//
+//----------**********----------//
+ 
+
+//----------**********----------//
+//----------BackEnd/admin/subject----------//
+//----------**********----------//
+
+
+//route of admin subject 
+//-----view page function
+Route::get('admin-subject', 'SubjectsController@admin_subject_index')->name('admin_subject_index');
+//-----call get,post function
+Route::get('admingetsubject', 'SubjectsController@admingetsubject')->name('admingetsubject');
+
+ 
+//route of subject create 
+Route::get('subject-create', 'SubjectsController@admin_create_subject_index')->name('admin_create_subject_index');
+//----------**********----------//
+
+ 
+//----------**********----------//
+//----------BackEnd/admin/user----------//
+//----------**********----------//
+
+//route of admin user 
+
+//-----view page function
 Route::get('admin-user', 'UserController@admin_user_index');
-
+//-----call get,post function
 Route::get('admingetuser', 'UserController@admingetuser')->name('admingetuser');
-//route of user create page
+//----------**********----------//
 
+//route of user create 
+
+//-----view page function
 Route::get('user-create', function () {
     return view('backend/admin/user/user-create');
 });
+
+//-----call get,post function
 Route::post('user', 'UserController@store')->name('user.store');
-
-
-//route of admin subject page
-Route::get('admin-subject', 'SubjectsController@admin_subject_index')->name('admin_subject_index');
-
-Route::get('admingetsubject', 'SubjectsController@admingetsubject')->name('admingetsubject');
-
-//route of subject create page
-Route::get('subject-create', 'SubjectsController@admin_create_subject_index')->name('admin_create_subject_index');
+//----------**********----------//
 
  
+//----------**********----------//
+//----------Test----------//
+//----------**********----------//
+
 //route TestController
+
+//-----view page function
 Route::get('test', 'TTTTestController@test');
+
+//-----call get,post function
+Route::get('test_create_subject', 'TTTTestController@test_create_subject');
+//----------**********----------//
