69177429e9e70c80e11cb753403199ced1318d76
kzan1410 <taweesaknuanjampa@gmail.com>
2021-08-16 01:36:21 +0700

    add test create subject

--- app/Http/Controllers/TTTTestController.php
+++ app/Http/Controllers/TTTTestController.php
@@ -1,23 +1,39 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 use App\Subjects;
 use Illuminate\Http\Request;
 
+use Auth;
+use DataTables;
+
+use App\User;
+
 use Validator;
 use Hash;
-use Auth;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class TTTTestController extends Controller {
-    
+
     public function test() {
-  
 
-      
- 
-        return $result;
+        return view( 'test' );
 
+    }
+
+    public function test_create_subject() {
+
+        $result = Subjects::create( [
+            'subject_code'=> 'subject -1',
+            'subject_title' => 'subject -1',
+            'subject_description_1' => 'subject_description_1<br>subject_description_1<br>subject_description_1<br>subject_description_1',
+            'subject_description_2' => 'subject_description_2',
+            'subject_description_3' => 'subject_description_3',
+        ] );
+
+        echo 'success';
     }
 }
